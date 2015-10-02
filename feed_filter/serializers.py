2d70d90eb5e00b256a2f2b52df3c8d6fa544a48c
nice-shot <shefferu@gmail.com>
2015-10-02 23:32:01 +0300

    added the update function but it still doesn't pass the test...

--- feed_filter/serializers.py
+++ feed_filter/serializers.py
@@ -2,6 +2,19 @@ from rest_framework import serializers
 from feed_filter.models import FacebookPage, Filter, Post, FilteredPost
 from django.contrib.auth.models import User
 
+def retrieve_page(page_data):
+    """
+    :param page_data: Details for a facebook page - keys are "id" and "name"
+    :type page_data: dict
+    :return: A facebook page that's either created or updated
+    :rtype: FaceBookPage
+    """
+    # This may cause a problem where some group names are overriden. Should find
+    # a smarter solution
+    return FacebookPage.objects.update_or_create(
+        defaults={"name": page_data["name"]},
+        id=page_data["id"]
+    )[0]
 
 class FacebookPageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,14 +30,22 @@ class FilterSerializer(serializers.ModelSerializer):
         pages_data = validated_data.pop("pages")
         new_filter = Filter.objects.create(**validated_data)
         for page_data in pages_data:
-            # This may cause a problem were some group names are overriden. We
-            # should find a smarter solution
-            page = FacebookPage.objects.update_or_create(
-                defaults={"name": page_data["name"]},
-                id=page_data["id"]
-            )[0]
-            new_filter.pages.add(page)
+            new_filter.pages.add(retrieve_page(page_data))
         return new_filter
+
+    def update(self, instance, validated_data):
+        #TODO: This isn't really working...
+        instance.user = validated_data.get("user", instance.user)
+        instance.name = validated_data.get("name", instance.name)
+        instance.filter_str = validated_data.get("filter_str",
+                                                 instance.filter_str)
+        if "pages" in validated_data:
+            instance.pages.clear()
+            for page_data in validated_data.get("pages", []):
+                instance.pages.add(retrieve_page(page_data))
+
+        return instance
+
 
 
 class FilteredPostSerializer(serializers.ModelSerializer):
--- feed_filter/tests.py
+++ feed_filter/tests.py
@@ -6,7 +6,7 @@ from .models import Filter
 class SerializersTests(TestCase):
     fixtures = ['single_user.json']
 
-    def test_filter_creation(self):
+    def test_filter_creation_and_update(self):
         """
         By creating the filter the FacebookPages should also be created
         """
@@ -23,3 +23,16 @@ class SerializersTests(TestCase):
         self.assertTrue(filter_serialize.is_valid())
         new_filter = filter_serialize.save()
         self.assertEqual(Filter.objects.count(), 1)
+        updated_data = {
+            "filter_str": '["friend", "of a friend"]',
+            "pages": [
+                {"id": "123124124", "name": "Bla"},
+                {"id": "421421", "name": "Wawa"},
+            ],
+        }
+        filter_serialize = FilterSerializer(new_filter, data=updated_data)
+        filter_serialize.is_valid()
+        print(filter_serialize.errors)
+        self.assertTrue(filter_serialize.is_valid())
+        updated_filter = filter_serialize.save()
+        self.assertEqual(updated_filter, new_filter)
