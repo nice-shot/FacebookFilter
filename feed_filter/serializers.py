c6e7ddd9d9edf0dae91b152aaf73ea40d9189c94
nice-shot <shefferu@gmail.com>
2015-10-02 13:10:45 +0300

    started creating the serializer for the Filter

--- feed_filter/serializers.py
+++ feed_filter/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
-from feed_filter.models import Filter, Post, FilteredPost
+from feed_filter.models import FacebookPage, Filter, Post, FilteredPost
 from django.contrib.auth.models import User
 
 
+class FacebookPageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FacebookPage
+
 class FilterSerializer(serializers.ModelSerializer):
+    pages = FacebookPageSerializer(many=True)
+
     class Meta:
         model = Filter
+
+    def create(self, validated_data):
+        # TODO - Add create and update methods to handle page creation
+        pages_data = validated_data.pop("pages")
+        feed_filter = Filter.objects.create
 
 class FilteredPostSerializer(serializers.ModelSerializer):
     class Meta:
