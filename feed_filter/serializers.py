8d97a0d5c3be54725e91a36a546b1e913501a4b7
nice-shot <shefferu@gmail.com>
2015-10-02 14:39:26 +0300

    the serializers still arent working. Gonna have to fix that and probably add a test

--- feed_filter/serializers.py
+++ feed_filter/serializers.py
@@ -14,9 +14,19 @@ class FilterSerializer(serializers.ModelSerializer):
         model = Filter
 
     def create(self, validated_data):
-        # TODO - Add create and update methods to handle page creation
         pages_data = validated_data.pop("pages")
-        feed_filter = Filter.objects.create
+        pages = []
+        for page_data in pages_data:
+            # This may cause a problem were some group names are overriden. We
+            # should find a smarter solution
+            # TODO: Make this work!
+            page = FacebookPage.objects.update_or_create(
+                defaults={"name": page_data["name"]},
+                id=page_data["id"]
+            )[0]
+            pages.append(page)
+        return Filter.objects.create(pages=pages, **validated_data)
+
 
 class FilteredPostSerializer(serializers.ModelSerializer):
     class Meta:
